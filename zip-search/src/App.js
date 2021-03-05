aa28847619c35574f622782adc847bf8dfa656ab
Jia Ming <jma8774@bths.edu>
2021-03-04 20:27:43 -0500

    assignment completed

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -2,26 +2,100 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-function City(props) {
-  return (<div>This is the City component</div>);
+function City({json}) { 
+  let splitCity = json.City.toLowerCase().split(' ');
+  for(var i = 0; i < splitCity.length; i ++) {
+    splitCity[i] = splitCity[i].charAt(0).toUpperCase() + splitCity[i].substring(1);
+  }
+  let capitalizeCity = splitCity.join(' ');
+
+  if(!json.EstimatedPopulation)
+    json.EstimatedPopulation = 'N/A';
+
+  return (
+  <div className="card mt-2 mb-2">
+      <div className="card-header">
+        <strong> {capitalizeCity} </strong>
+      </div>
+      <div className="card-body">
+        <ul>
+          <li> State: {json.State} </li>
+          <li> Location: {json.Lat}, {json.Long} </li>
+          <li> Estimated Population: {json.EstimatedPopulation} </li>
+        </ul>
+      </div>
+  </div>
+  );
 }
 
-function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+function ZipSearchField({ onZipChange }) {
+  return (
+      <div className="mt-2">
+        <label className='mr-2'> <strong> Zip Code: </strong> </label>
+        <input type="text" onChange={onZipChange}/>
+      </div>
+  );
 }
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zipCode: '',
+      cities: [],
+    }
+  }
+
+  zipChanged(e) {
+    let zip = e.target.value;
+
+    // Conditional for not found when length is 0 and it is not a number
+    if(e.target.value.length !== 5 || isNaN(e.target.value)) {
+      this.setState({
+        zipCode: '',
+        cities: [],
+      })
+      return
+    }
+
+    fetch(`https://ctp-zip-api.herokuapp.com/zip/${zip}`)
+      .then(res => res.json())
+      .then(jsonData => {
+        this.setState({
+          cities: jsonData
+        })
+      })
+      .catch(error => {
+        console.log("Invalid Zip Code!");
+      });
+
+    this.setState({
+      zipCode: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
-        <div>
-          <City />
-          <City />
+        <div className="container pt-2">
+          <ZipSearchField onZipChange={(e) => this.zipChanged(e)} />
+          <div>
+            {
+              (this.state.cities.length === 0
+                ? <div className='mt-2'> No results found for the above zip code. </div>
+                : this.state.cities.map(c => {
+                    return <City 
+                      key={c.RecordNumber} 
+                      json={c}
+                    />
+                  })
+              )
+            }
+          </div>
         </div>
       </div>
     );
